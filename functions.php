585a20e6194492b24322b430d63af51561acb7a6
Tom Rees <zephod@gmail.com>
2012-01-17 14:21:52 +0000

    [register-form]: Re-added blurb to registration form.

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 add_filter('bp_sidebar_login_form', 'filter_login_form');
 add_filter('wp_nav_menu', 'filter_nav_menu'); 
 add_filter('comment_form', 'filter_comment_form'); 
+add_filter('bp_before_account_details_fields', 'register_form_blurb'); 
 
 /*
  * Use the simple_html_dom library to perform 
@@ -56,19 +57,12 @@ function filter_comment_form() {
   // Hide the ugly existing button
   echo '<style type="text/css">input[type="submit"]#submit { display: none; }</style>';
 }
-							
-
-function filter_buttons_primary($widget) {
-  // Add 'class="btn"'
-  $widget = preg_replace('/(type="submit")/i', '$1 class="btn primary"', $widget);
-  return $widget;
-}
 
 function test($a) {
   echo "<br><br><hr><pre style=\"background: #fcf;\">".htmlspecialchars($a)."</pre><hr><br><br>";
 }
 
-/* Taken from buddypress:functions.php.bp_dtheme_main_nav */
+/* Taken from buddypress:functions.php.bp_dtheme_main_nav. Used by header.php */
 function okfn_fallback_nav_menu( $args ) {
 	global $bp;
 
@@ -83,6 +77,12 @@ function okfn_fallback_nav_menu( $args ) {
 	echo $menu;
 
 	do_action( 'bp_nav_items' );
+}
+
+function register_form_blurb( $args ) {
+  echo "<p>Community Membership is a way of &quot;opting in&quot; and publicly acknowledging a connnection with the Open Knowledge Foundation and support for its activities."
+    ." It entails no specific obligations (nor confers specific rights!) and anyone may join.</p>"
+    ."<p><a href=\"/governance/#community-membership\">Read more about Community Membership &raquo;</a></p>";
 }
 
 
