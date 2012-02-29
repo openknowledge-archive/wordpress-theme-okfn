e4daf496e78ff9ba6898ee8c4ce95c2a8b19ecfb
Sam Smith <sam@mintcanary.com>
2012-02-29 17:41:26 +0000

    ie fixes

--- single.php
+++ single.php
@@ -54,7 +54,7 @@
 
   <div class="author-box">
     <?php echo get_avatar( get_the_author_meta( 'user_email' ), '218' ); ?>
-    <h3><?php printf( _x( 'About %s', 'Post written by...', 'buddypress' ), str_replace( '<a href=', '<a rel="author" href=', bp_core_get_userlink( $post->post_author ) ) ); ?></h3>
+    <h3><?php printf( _x( 'Written by %s', 'Post written by...', 'buddypress' ), str_replace( '<a href=', '<a rel="author" href=', bp_core_get_userlink( $post->post_author ) ) ); ?></h3>
     <p><?php the_author_description(); ?></p>
   </div>	
 	
