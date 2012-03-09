68068028ffe5db1fe9970befb42fa1760da10d4c
Tom Rees <zephod@gmail.com>
2012-03-09 15:07:04 +0000

    [js]: Stripped unused JS files.

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
     </script>
 
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo bloginfo('stylesheet_directory'); ?>/js/app.js"></script>
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
