8931eea0007fef44e806e46387150196660225d1
Sam Smith <sam@mintcanary.com>
2012-04-04 17:00:53 +0100

    carousel shortcode addded

--- shortcodes.php
+++ shortcodes.php
@@ -1,9 +1,11 @@
 <?php
-/*************************************************************
-* Shortcode:   Pseudo Sidebar
+/*****************************************************************
+* Name:        Pseudo Sidebar
 * Author:      Sam Smith
 * Description: Use to insert a sidebar into a single column page
-**************************************************************/
+* Example:     [pseudocontent] ...Left Content... [/pseudocontent]
+*              [pseudosidebar] ...Right Content... [/pseudosidebar]
+*******************************************************************/
 
 function pseudocontent_shortcode( $atts, $content = null ) {
    return '<style type="text/css">#content {width: 100%;} #content #sidebar h5 {margin-top:0px;}</style>
@@ -15,5 +17,44 @@ function pseudosidebar_shortcode( $atts, $content = null ) {
    return '<div id="sidebar" role="complementary" style="margin-top:-56px;">' . $content . '</div>';
 } 
 add_shortcode( 'pseudosidebar', 'pseudosidebar_shortcode' );
+
+
+
+/**********************************************************************************
+* Name:        Carousel
+* Author:      Sam Smith
+* Description: Use to insert a Bootstrap carousel
+* Example:     [carousel]
+               [slide img="http://slide1.jpg" caption="Caption One" class="active"]
+							 [slide img="http://slide2.jpg" caption="Caption Two"]
+							 [/carousel]
+***********************************************************************************/
+
+function carousel_shortcode( $atts, $content = null ) {
+	
+   return '<div id="myCarousel" class="carousel"><div class="carousel-inner">' .do_shortcode($content). '</div><a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a><a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a></div>';
+} 
+add_shortcode( 'carousel', 'carousel_shortcode' );
+
+function carousel_slide_shortcode( $atts ) {  
+	extract( shortcode_atts( array(
+			'img' => 'http://farm8.staticflickr.com/7174/6554801385_83acdc501d_o_d.png',
+			'caption' => '',
+			'class' => '',
+		), $atts ) );
+		
+		if(!empty($caption)){
+			return '<div class="item ' .$class. '"><img src="' .$img. '">
+				<div class="carousel-caption">
+					<p>' .$caption. '</p>
+				  </div>
+				</div>'; 
+		}
+		else {
+			return '<div class="item"><img src="' .$img. '">
+			  </div>'; 
+		}
+}  
+add_shortcode('slide', 'carousel_slide_shortcode');  
 
 ?>
