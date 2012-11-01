dd44c77c0008a6a8b848e77643507f9a0db04839
Michael Bauer <mihi@lo-res.org>
2012-11-01 16:15:03 +0100

    added is_tree function for determining the root of subpages
    I'll need this for widget logic and the Scoda handbook

--- functions.php
+++ functions.php
@@ -796,4 +796,23 @@ if ( !function_exists('base_rss_feed') ) {
 
 include('shortcodes.php');
 
+
+function is_tree( $pid ) {      // $pid = The ID of the page we're looking for
+pages underneath
+    global $post;               // load details about this page
+
+    if ( is_page($pid) )
+        return true;            // we're at the page or at a sub page
+
+    $anc = get_post_ancestors( $post->ID );
+    foreach ( $anc as $ancestor ) {
+        if( is_page() && $ancestor == $pid ) {
+            return true;
+        }
+    }
+
+    return false;  // we arn't at the page, and the page is not an ancestor
+}
+
+
 ?>
