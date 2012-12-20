b5e8df0ce3a61e9028a43c96c13e5f1e827bcfe9
Sam Smith <sam@mintcanary.com>
2012-12-20 17:15:59 +0000

    tabbed mailing list bar

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
       foreach ($options as $value) {
           if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
       }
-      if ( $okfn_mailinglist_bar == "true" && $okfn_mailinglist_bar_location == "footer" ) : ?>
+      if ( $okfn_mailinglist_bar == "true" && $okfn_mailinglist_bar2 == "false" && $okfn_mailinglist_bar_location == "footer" ) : ?>
       <section class="subscribe">
         <div class="container">
           <? if ( $okfn_mailinglist_bar_type == "mailchimp") : ?>
@@ -73,6 +73,123 @@
          <? endif; ?>
         </div>
       </section>
+    <? elseif ( $okfn_mailinglist_bar == "true" && $okfn_mailinglist_bar2 == "true" && $okfn_mailinglist_bar_location == "footer" ) : ?>
+      <div class="tabbable mailing-lists"> 
+      	<div class="container">
+          <ul class="nav nav-tabs">
+            <li class="active"><a href="#tab1" data-toggle="tab"><? if (!empty($okfn_mailinglist_heading)) { echo $okfn_mailinglist_heading;} else { echo 'Mailing List One';} ?></a></li>
+            <li><a href="#tab2" data-toggle="tab"><? if (!empty($okfn_mailinglist_heading2)) { echo $okfn_mailinglist_heading2;} else { echo 'Mailing List Two';} ?></a></li>
+          </ul>
+        </div>
+        <section class="subscribe">
+          <div class="tab-content container">
+            <div class="tab-pane active" id="tab1">
+              <? if ( $okfn_mailinglist_bar_type == "mailchimp") : ?>
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                  <form action="<?php echo $okfn_mailinglist_action?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <table width="100%">
+                      <tr>
+                        <th scope="row"><? if (!empty($okfn_mailinglist_description)) { echo $okfn_mailinglist_description;} ?></th>
+                        <td><label for="mce-EMAIL">Email Address </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address"></td>
+                        <td><label for="mce-FNAME">First Name </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name"></td>
+                        <td><label for="mce-LNAME">Last Name </label>
+                            <input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name"></td>
+                        <td class="submit"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></td>
+                      </tr>
+                    </table>
+                    <div id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>
+                  </form>
+                </div>
+                <!--End mc_embed_signup-->
+                <?php elseif ( $okfn_mailinglist_bar_type == "mailman") : ?>
+                <form method="post" action="<?php echo $okfn_mailinglist_action?>">
+                  <table width="100%">
+                    <tr>
+                      <th scope="row"><? if (!empty($okfn_mailinglist_description)) { echo $okfn_mailinglist_description;} ?></th>
+                      <td>
+                        <label><? echo __("Name", "okfn")?></label>
+                        <input name="fullname" placeholder="<? echo __("Name", "okfn")?>" type="text">
+                      </td>
+                      <td>
+                        <label><? echo __("Email Address", "okfn")?></label>
+                        <input name="email" placeholder="<? echo __("Email Address", "okfn")?>" type="email">
+                      </td>
+                      <td class="announce">
+                        <label class="checkbox">
+                          <input type="checkbox" value="" disabled>
+                          <? echo __("Receive newsletter", "okfn")?>
+                        </label>
+                      </td>
+                      <td class="submit">
+                        <input type="submit" name="email-button" value="<? echo __("Subscribe", "okfn")?>" class="button">
+                      </td>
+                    </tr>
+                  </table>
+                </form>
+               <? endif; ?>
+            </div>
+            <div class="tab-pane" id="tab2">
+              <? if ( $okfn_mailinglist_bar_type2 == "mailchimp") : ?>
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                  <form action="<?php echo $okfn_mailinglist_action2?>" method="post" id="mc-embedded-subscribe-form2" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <table width="100%">
+                      <tr>
+                        <th scope="row"><? if (!empty($okfn_mailinglist_description2)) { echo $okfn_mailinglist_description2;} ?></th>
+                        <td><label for="mce-EMAIL">Email Address </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address"></td>
+                        <td><label for="mce-FNAME">First Name </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name"></td>
+                        <td><label for="mce-LNAME">Last Name </label>
+                            <input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name"></td>
+                        <td class="submit"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></td>
+                      </tr>
+                    </table>
+                    <div id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>
+                  </form>
+                </div>
+                <!--End mc_embed_signup-->
+                <?php elseif ( $okfn_mailinglist_bar_type2 == "mailman") : ?>
+                <form method="post" action="<?php echo $okfn_mailinglist_action?>">
+                  <table width="100%">
+                    <tr>
+                      <th scope="row"><? if (!empty($okfn_mailinglist_description2)) { echo $okfn_mailinglist_description2;} ?></th>
+                      <td>
+                        <label><? echo __("Name", "okfn")?></label>
+                        <input name="fullname" placeholder="<? echo __("Name", "okfn")?>" type="text">
+                      </td>
+                      <td>
+                        <label><? echo __("Email Address", "okfn")?></label>
+                        <input name="email" placeholder="<? echo __("Email Address", "okfn")?>" type="email">
+                      </td>
+                      <td class="announce">
+                        <label class="checkbox">
+                          <input type="checkbox" value="" disabled>
+                          <? echo __("Receive newsletter", "okfn")?>
+                        </label>
+                      </td>
+                      <td class="submit">
+                        <input type="submit" name="email-button" value="<? echo __("Subscribe", "okfn")?>" class="button">
+                      </td>
+                    </tr>
+                  </table>
+                </form>
+               <? endif; ?>
+            </div>
+          </div>
+        </section>
+      </div>
+
+     
     <? endif; ?>
 
     <footer>
